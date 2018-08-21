7c85c17941b04970c2737e98d5eb6952d0e10d45
devyogilogic <yogeshyadavcsk@gmail.com>
2018-08-21 22:25:15 +0530

    yyryyr
    htyrtryr

---
+++ armstrong.js
@@ -0,0 +1,21 @@
+function armstr()
+{
+var arm=0,a,b,c,d,num;
+num=Number(process.argv[2]);
+
+temp=num;
+while(temp>0)
+{
+a=temp%10;
+temp=parseInt(temp/10); 
+arm=arm+a*a*a;
+}
+if(arm==num)
+console.log("Armstrong number");
+}
+else
+{
+console.log("Not Armstrong number");
+}
+}
+armstr()
---
+++ primenumber.js
@@ -0,0 +1,23 @@
+function prime()
+{
+    var  number =Number(process.argv[2])
+    var flag=Number('123')
+    console.log( flag)
+    for( var i=2; i<=number/2;++i){
+        if(number%i==0){
+           
+flag=1;
+break;
+        }
+
+
+    }
+    if(flag==0){
+        console.log("  prime :)")
+
+    }
+    else {
+        console.log("not");
+    }
+}
+prime();
