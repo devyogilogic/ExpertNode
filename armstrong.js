9e324796bebd9286421804a90a1e555256976fe3
devyogilogic <yogeshyadavcsk@gmail.com>
2018-08-22 21:20:57 +0530

    fggg

--- armstrong.js
+++ armstrong.js
@@ -1,22 +1,35 @@
 function armstr()
 {
 var arm=0,a,b,c,d,num;
-num=Number(process.argv[2]);
+var count=Number(0)
+var result=[]
+var i=Number(0)
+num=Number(process.argv[2])
+var number2=Number(process.argv[3])
+for(i= num;i<=number2;++i){
 
-temp=num;
-while(temp>0)
+    temp=i;
+    while(temp>0)
+    {
+    a=temp%10;
+    temp=parseInt(temp/10); 
+    arm=arm+a*a*a;
+    
+    }
+   let ad = arm
+   arm=0 
+    if(ad==i)
 {
-a=temp%10;
-temp=parseInt(temp/10); 
-arm=arm+a*a*a;
+result[count]=i
+console.log(result[count])
+++count
+
+
 }
-if(arm==num)
-{
-console.log("Armstrong number");
+    
 }
-else
-{
-console.log("Not Armstrong number");
-}
+
+
+
 }
 armstr()
